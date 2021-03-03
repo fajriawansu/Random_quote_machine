b94fc6917d6d450514b4f79f3f622a82d4d66af3
Fajriawan Sutansa <f.sutansa46@gmail.com>
2021-03-03 13:14:20 +0700

    Add random quote machine

---
+++ README.md
@@ -0,0 +1,1 @@
+## [A random quote machine]{https://fajriawansu.github.io/a_random_quote_machine }
---
+++ quotebank.js
@@ -0,0 +1,117 @@
+const QUOTEBANK = [
+  {
+    quote:"Percaya, kamu punya sesuatu yang berbeda. Mereka yang lebih berhasil darimu belum tentu lebih hebat dalam bahagia.",
+    author: "ig: @kjdb.kjdb"
+  },{
+    quote:"Nikmat yang sering lupa disyukuri adalah nikmat untuk bisa melupakan.",
+    author: "ig: @kjdb.kjdb"
+  },{
+    quote:"Kamu cuma berlama-lama pada sesuatu yang meninggalkanmu sejak lama. Ikhlaskan atau harimu sia-sia.",
+    author: "ig: @kjdb.kjdb"
+  },{
+    quote:"Barangkali, kamu dibuat kuat supaya bisa menguatkan orang lain. Bukan malah menjatuhkan dengan bilang 'gitu doang gak bisa'.",
+    author: "ig: @kjdb.kjdb"
+  },{
+    quote:"Berusaha dulu, jangan abaikan sakit hatimu. Mecoba jadi lebih baik berbeda dengan mencoba baik-baik saja.",
+    author: "ig: @kjdb.kjdb"
+  },{
+    quote:"Beberapa orang akan mengerti walau kamu tidak bicara. Beberapa orang tidak akan mengerti walau kamu panjang menjelaskan.",
+    author: "ig: @kjdb.kjdb"
+  },{
+    quote:"Terkadang, beberapa orang pergi tanpa kejelasan. Dan terkadang kamu cuma perlu biarkan mereka.",
+    author: "ig: @kjdb.kjdb"
+  },{
+    quote:"Jangan takut kehilangan seseorang, takutlah untuk memaksakan diri sesuai omongan orang lain. Kamu kehilangan dirimu.",
+    author: "ig: @kjdb.kjdb"
+  },{
+    quote:"Bukannya Tuhan tidak tahu remuknya hati, bukannya Tuhan tidak mau peduli. Soal sakit ini, Dia tahu kita mampu hadapi.",
+    author: "ig: @kjdb.kjdb"
+  },{
+    quote:"Kata 'teman' juga perlu pembuktian. Ketika kamu ada tapi hatinya tetap merasa sendiri, kamu belum benar-benar menemani.",
+    author: "ig: @kjdb.kjdb"
+  },{
+    quote:"Tenang, akan selalu tiba hari di mana semua kembali seperti semua.",
+    author: "ig: @kjdb.kjdb"
+  },{
+    quote:"Cinta yang diungkapkan tanpa persiapan adalah bukti kamu tidak benar-benar mencintainya.",
+    author: "ig: @kjdb.kjdb"
+  },{
+    quote:"Semua punya standar kebahagiaan masing-masing yang gak bisa dipaksa sama.",
+    author: "ig: @kjdb.kjdb"
+  },{
+    quote:"Kalau sudah berjalan dengan iman, langkah berat akan terasa ringan. Karena kamu percaya kamu mampu, kamu percaya Tuhan akan bantu.",
+    author: "ig: @kjdb.kjdb"
+  },{
+    quote:"Bukan dengan banyaknya teman yang dia punya, lelaki sejati adalah dia yang mampu menjaga tanpa kehilangan.",
+    author: "ig: @kjdb.kjdb"
+  },{
+    quote:"Jangan paksakan standar bahagiamu sesuai dengan apa yang menurutmu bisa membuat orang lain juga bahagia.",
+    author: "ig: @kjdb.kjdb"
+  },{
+    quote:"Jika sering merasa terlambat dalam memulai sesuatu, biasanya itu karena kamu membandingkan dengan pencapaian orang lain.",
+    author: "ig: @kjdb.kjdb"
+  },{
+    quote:"Jangan bandingkan dirimu dengan pencapaian orang lain, kamu akan selalu merasa terlambat dalam memulai dan tidak akan memulai.",
+    author: "ig: @kjdb.kjdb"
+  },{
+    quote:"Mencapai mimpi itu melelahkan, anehnya ketika kita bisa mencapai satu mimpi, kita tetap terus menambah mimpi yang lain.",
+    author: "ig: @kjdb.kjdb"
+  },{
+    quote:"Tenang, semua yang tetap milikmu akan kembali bagaimanapun caranya nanti.",
+    author: "ig: @kjdb.kjdb"
+  },{
+    quote:"Sabar, hari ini gak semua hal harus bisa kamu kejar.",
+    author: "ig: @kjdb.kjdb"
+  },{
+    quote:"Kasih ruang untuk orang lain, dunia tidak tercipta hanya untukmu.",
+    author: "ig: @kjdb.kjdb"
+  },{
+    quote:"Alangkah mengerikannya menjadi tua dengan kenangan masa muda yang hanya berisi kemacetan jalan dengan uang pensiun tidak seberapa.",
+    author: "Seno Gumira A"
+  },{
+    quote:"Seorang pencerita adalah juga seorang penghapus.",
+    author: "Intan Paramadhita"
+  },{
+    quote:"Aku kira, setiap penulis yang jujur, akhir-kelaknya akan kecewa dan dikecewakan.",
+    author: "Pramoedya AT"
+  },{
+    quote:"Orang boleh pandai setinggi langit, tapi selama ia tidak menulis, ia akan hilang di dalam masyarakat dan dari sejarah. Menulis adalah bekerja untuk keabadian.",
+    author: "Pramoedya AT"
+  },{
+    quote:"Dalam hidup kita, cuma satu yang kita punya, yaitu keberanian. Kalau tidak punya itu, lantas apa harga hidup kita ini?",
+    author: "Pramoedya AT"
+  },{
+    quote:"Definisi kesepian yang sebenarnya adalah hidup tanpa tanggung jawab sosial.",
+    author: "Goenawan Mohamad"
+  },{
+    quote:"Kemampuan membaca itu sebuah rahmat. Kegemaran membaca: sebuah kebahagiaan.",
+    author: "Goenawan Mohamad"
+  },{
+    quote:"Kau harus tahu lupa adalah lahan subur kenangan-kenangan. Biarkan ia mengalir seumpama sungai. Saatnya akan tiba, kau akan betul-betul lupa.",
+    author: "M Aan Mansyur"
+  },{
+    quote:"Aku mencintaimu. Itu sebabnya aku takkan pernah selesai mendoakan keselamatanmu.",
+    author: "Sapardi Djoko Damono"
+  },{
+    quote:"Lalu, apabila kematian adalah keperkasaan kodrati maka kehadirannya, bahkan baru gejalanya, sudah mampu membungkam segala gejolak rasa.",
+    author: "Ahmad Tohari"
+  },{
+    quote:"Semakin banyak yang kuketahui tentang dirinya, semakin ia tak menarik lagi. Hubungan kita bisa panjang, karena aku tak tahu seluruhnya tentang dirimu.",
+    author: "Linda Christanty"
+  },{
+    quote:"Kita bisa memesan bir, namun tidak bisa memesan takdir.",
+    author: "Djenar Maesa Ayu"
+  },{
+    quote:"Adakah keindahan perlu dinamai?",
+    author: "Ayu Utami"
+  },{
+    quote: "Jika orang lain menghina Anda, Anda mungkin melupakan penghinaan itu; tapi jika Anda menghina orang lain, Anda akan selalu mengingatnya.",
+    author: "Kahlil Gibran"
+  },{
+    quote: "Sepasang kekasih pada akhirnya tidak bertemu di suatu tempat. Mereka sebenarnya telah ada dalam diri satu sama lain selama ini.",
+    author: "Jalaluddin Rumi"
+  },{
+    quote: "Masa lalu tidak pernah hilang; Ia ada tetapi tidak tahu jalan pulang. Untuk itu ia menitipkan surat kepada sesuatu yang tidak kita duga. Kita menyebutnya kenangan.",
+    author: "M Aan Mansyur"
+  }
+]
